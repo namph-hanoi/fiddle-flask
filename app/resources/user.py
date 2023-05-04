5179df19d134fe606e363b3be012c6ddb3a6b83b
Nam Phan <namph.tech@gmail.com>
2023-05-04 08:59:01 +0000

    ✨ User register function

--- app/resources/user.py
+++ app/resources/user.py
@@ -1,6 +1,12 @@
 from flask.views import MethodView
+from flask import abort
 from flask_smorest import Blueprint
 from injector import Injector
+from app.database import db
+from sqlalchemy import or_
+from app.models import UserModel
+from passlib.hash import pbkdf2_sha256
+from app.schemas.user import UserRegisterSchema
 
 injector = Injector()
 
@@ -8,10 +14,31 @@ injector = Injector()
 blp = Blueprint("users", "users", url_prefix="/user", description="Operations on users")
 # Todo: redis cache
 
-
+#  Todo: inject model
 @blp.route("/register")
 class UserRegister(MethodView):
-    def post(self): 
+    @blp.arguments(UserRegisterSchema)
+    def post(self, user_data):
+        try:
+            if UserModel.query.filter(
+                or_(
+                    UserModel.username == user_data["username"],
+                    UserModel.email == user_data["email"],
+                )
+            ).first():
+                abort(409, message="A user with that username or email already exists.")
+
+            user = UserModel(
+                username=user_data["username"],
+                email=user_data["email"],
+                # Todo: create a class function for hash inside User model
+                password=pbkdf2_sha256.hash(user_data["password"]),
+            )
+            db.session.add(user)
+            db.session.commit()
+        except Exception as error:
+            print("Error occured:", error)
+            
         return {"message": "User created successfully."}, 201
     
 
