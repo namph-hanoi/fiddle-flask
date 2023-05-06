ad06daf361e4cdb9c9604d802ff608bb50b315d3
Nam Phan <namph.tech@gmail.com>
2023-05-06 04:51:19 +0000

    ✨ Remove try_except block, smorest.abort already does the work

--- app/resources/user.py
+++ app/resources/user.py
@@ -12,7 +12,7 @@ from flask_jwt_extended import (create_refresh_token, create_access_token)
 injector = Injector()
 
 
-blp = Blueprint("users", "users", url_prefix="/user", description="Operations on users")
+blp = Blueprint("users", "users", url_prefix="/api/v1/user", description="Operations on users")
 # Todo: redis cache
 
 #  Todo: inject model
@@ -20,28 +20,23 @@ blp = Blueprint("users", "users", url_prefix="/user", description="Operations on
 class UserRegister(MethodView):
     @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
-        try:
-            if UserModel.query.filter(
-                or_(
-                    UserModel.username == user_data["username"],
-                    UserModel.email == user_data["email"],
-                )
-            ).first():
-                abort(409, message="A user with that username or email already exists.")
-
-            user = UserModel(
-                username=user_data["username"],
-                email=user_data["email"],
-                # Todo: create a class function for hash inside User model
-                password=pbkdf2_sha256.hash(user_data["password"]),
+        if UserModel.query.filter(
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"],
             )
-            db.session.add(user)
-            # Todo: create decorator to auto commit or rollback
-            db.session.commit()
-        #  Todo: may not need error exception
-        except Exception as error:
-            print("Error occured:", error)
-            
+        ).first():
+            abort(409, message="A user with that username or email already exists.")
+
+        user = UserModel(
+            username=user_data["username"],
+            email=user_data["email"],
+            # Todo: create a class function for hash inside User model
+            password=pbkdf2_sha256.hash(user_data["password"]),
+        )
+        db.session.add(user)
+        # Todo: create decorator to auto commit or rollback
+        db.session.commit()            
         return {"message": "User created successfully."}, 201
     
 
